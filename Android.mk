de401ed71da813bcd13da60c26ae6e60e6c2eba4
R0rt1z2 <rogerortizleal@gmail.com>
2022-12-02 11:08:26 +0200

    hisi-nve: add basic implementation for memmem()

--- Android.mk
+++ Android.mk
@@ -6,11 +6,13 @@ LOCAL_MODULE := hisi-nve
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/src/nve/include \
-    $(LOCAL_PATH)/src/crypto/include
+    $(LOCAL_PATH)/src/crypto/include \
+    $(LOCAL_PATH)/src/memory/include
 
 LOCAL_SRC_FILES := \
     src/nve/hisi_nve.c \
-    src/crypto/sha256.c
+    src/crypto/sha256.c \
+    src/memory/memory.c
 
 LOCAL_CFLAGS += \
     -Wno-pointer-sign \
---
+++ src/memory/include/memory.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void *memmem (const void *haystack_start, size_t haystack_len,
+          const void *needle_start, size_t needle_len);
